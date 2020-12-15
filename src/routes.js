83ae0f2605fd8b5965429878660204dd4498fb87
Lucky <janusr777@gmail.com>
2020-12-15 10:56:54 +0100

    crete routes

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+class Routes extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      
+      </BrowserRouter>
+    )
+  }
+}
+
+export default Routes;
