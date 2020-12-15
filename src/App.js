bb0434fac38d3fd21a4f179a2c2876cfe43ce9dd
Lucky <janusr777@gmail.com>
2020-12-15 11:11:10 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  } from "./";
+import Routes from "./routes";
 
 const App = () => {
     return (
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Home from "./components/home";
+import Login from "./components/login";
+import Contact from "./components/contact";
+
 class Routes extends Component {
   render() {
     return (
       <BrowserRouter>
-      
+        <Header />
+          <Switch>
+            <Route to="/" component={Home} exact />
+            <Route to="/login" component={Login} exact />
+            <Route to="/contact" component={Contact} exact />
+          </Switch>
+        <Footer />
       </BrowserRouter>
     )
   }
